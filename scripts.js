024d17589ab80e792073b4c79aa59ed3db62c9ae
ramyhijazi <ramydhtr@gmail.com>
2024-07-27 14:28:25 -0500

    Completed all JS calculator  functions

--- scripts.js
+++ scripts.js
@@ -6,25 +6,56 @@
 
 // Step 4: Equals sign is pressed, make calculation
 
-
 let firstNumber;
 let secondNumber;
+let step = 0; // Variable to keep track of the step that we are on
+let operation;
+let result = 0;
 
-
-let operation
+const display = document.getElementById("display");
+let numArray = []; // Create an array to keep track of all the buttons pressed.
+// Every time we press a number, it will be inserted into this array for later.
+let secondNumArray = [];
 
 function getNumber(num) {
-    console.log(num)
+  if (step === 0 || step === 1) {
+    numArray.push(num); // Numbers will be added into the array
+    step = 1;
+    firstNumber = Number(numArray.join("")); // Join all inputs into one Number
+    display.value = firstNumber;
+  } else if (step === 2) {
+    // Same thing here but for the second number the user enters.
+    secondNumArray.push(num);
+    secondNumber = Number(secondNumArray.join(""));
+    display.value = secondNumber;
+  }
 }
 
-function getOperator(op){
-    console.log(op)
+function getOperator(op) {
+  step = 2;
+  operation = op; // The operation that we passed in our HTML file
 }
 
 function clearDisplay() {
-    console.log('clear display')
+  display.value = 0;
+  firstNumber = null;
+  secondNumber = null;
+  step = 0;
+  operation = null;
+  result = 0;
+  numArray = [];
+  secondNumArray = [];
 }
 
 const calculateEquals = () => {
-    console.log("calculate")
-}
+  if (operation === "+") {
+    result = firstNumber + secondNumber;
+  } else if (operation === "-") {
+    result = firstNumber - secondNumber;
+  } else if (operation === "/") {
+    result = firstNumber / secondNumber;
+  } else if (operation === "*") {
+    result = firstNumber * secondNumber;
+  }
+  display.value = result;
+};

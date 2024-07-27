4af3f8667384077ad1a4efa577bd7d0c4abb06ad
ramyhijazi <ramydhtr@gmail.com>
2024-07-27 13:30:39 -0500

    Created global functions and variables

--- scripts.js
+++ scripts.js
@@ -5,3 +5,26 @@
 // Step 3: Save the second number that was pressed
 
 // Step 4: Equals sign is pressed, make calculation
+
+
+let firstNumber;
+let secondNumber;
+
+
+let operation
+
+function getNumber(num) {
+    console.log(num)
+}
+
+function getOperator(op){
+    console.log(op)
+}
+
+function clearDisplay() {
+    console.log('clear display')
+}
+
+const calculateEquals = () => {
+    console.log("calculate")
+}

160424e56e2eea3c78bd508ffadb0c5e397ae1a3
ramyhijazi <ramydhtr@gmail.com>
2024-07-27 13:29:00 -0500

    Fixed buttons and added respective functions

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Step 1: Save the number that has been pressed
+
+// Step 2: Save the operator that was pressed
+
+// Step 3: Save the second number that was pressed
+
+// Step 4: Equals sign is pressed, make calculation
